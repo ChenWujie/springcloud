fd0ac7ec961ab6f3e947f7050e49d903dce19437
chenwujie <wujie.chen@qq.com>
2021-06-22 23:34:28 +0800

    添加消费者模块，通过消费者模块调用生产者，并进行项目重构，将公共部分代码提取到cloud-api-commons模块

--- cloud-api-commons/src/main/java/com/cwj/springcloud/entities/CommonResult.java
+++ cloud-api-commons/src/main/java/com/cwj/springcloud/entities/CommonResult.java
@@ -16,4 +16,3 @@ public class CommonResult<T>{
         this(code, message, null);
     }
 }
-
--- cloud-provider-payment8001/src/main/java/com/cwj/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/cwj/springcloud/controller/PaymentController.java
@@ -17,14 +17,13 @@ public class PaymentController{
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment)
+    public CommonResult create(@RequestBody Payment payment)
     {
         int result = paymentService.create(payment);
-        log.info("*****插入结果："+result);
 
         if(result > 0)
         {
-            return new CommonResult(200,"插入数据库成功,serverPort: ");
+            return new CommonResult(200,"插入数据库成功,serverPort: ", payment);
         }else{
             return new CommonResult(444,"插入数据库失败",null);
         }
@@ -37,7 +36,7 @@ public class PaymentController{
 
         if(payment != null)
         {
-            return new CommonResult(200,"查询成功,serverPort:  ");
+            return new CommonResult(200,"查询成功,serverPort:  ", payment);
         }else{
             return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
         }
