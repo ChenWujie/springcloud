c8f95567beeb8b1165beca9079a3760a5be73493
chenwujie <wujie.chen@qq.com>
2021-06-24 15:04:14 +0800

    修改Ribbon的轮询算法，将默认的一台机器一次改成随机选

--- cloud-consumer-order80/src/main/java/com/cwj/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/cwj/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package com.cwj.springcloud;
 
+import com.cwj.rule.MyRibbonRuler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "cloud-payment-service", configuration = MyRibbonRuler.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
